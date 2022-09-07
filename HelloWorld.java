a4f7ebf5c4b8b01d1bae23b3129ace94b1f8eba1
Ajinkya1810 <ajinkyatikhe18@gmail.com>
2022-09-07 19:01:58 +0530

    Commit demo - 1

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,6 @@ public class HelloWorld {
     public static void main(String[] args){
 
         System.out.printf("Hello-World");
-        System.out.println("Ajinnkya");
+        System.out.println("Ajinykya");
     }
 }
